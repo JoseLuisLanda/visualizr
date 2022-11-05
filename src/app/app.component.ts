8810fb9893203d8950534312e983c75f13c556cf
JoseLuisLanda <viajero13279@innovaccion.mx>
2022-11-05 00:13:37 -0600

    Rebase and accept merging

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,7 +10,7 @@ export class AppComponent implements OnInit, OnChanges  {
   @Output() item: EventEmitter<ElementId> = new EventEmitter<ElementId>();
   switchTemp: boolean  = false;
   title = 'Visualiz-AR';
-  itemAR:ElementId={uid:"Astronaut",name:"https://firebasestorage.googleapis.com/v0/b/visualizr.appspot.com/o/3DModels%2Flion_sculpture_processed.glb?alt=media&token=00e3d0fc-0744-4108-8f5a-702273c5471d"};
+  itemAR:ElementId={uid:"Astronaut",name:"https://firebasestorage.googleapis.com/v0/b/visualizr.appspot.com/o/3DModels%2Faltar_de_dia_de_muertos.glb?alt=media&token=234a9642-8602-4887-a03b-b234e39b51f9"};
   ngOnChanges(changes: SimpleChanges): void {
     throw new Error('Method not implemented.');
   }
--- src/app/shared/arelement/arelement.component.ts
+++ src/app/shared/arelement/arelement.component.ts
@@ -8,7 +8,7 @@ import { ElementId } from 'src/app/collections/element';
 })
 export class ARElementComponent implements OnInit {
   @Input() item: ElementId = 
-  {name:"https://firebasestorage.googleapis.com/v0/b/visualizr.appspot.com/o/3DModels%2Flion_sculpture_processed.glb?alt=media&token=00e3d0fc-0744-4108-8f5a-702273c5471d"} as ElementId;
+  {name:"https://firebasestorage.googleapis.com/v0/b/visualizr.appspot.com/o/3DModels%2Fthe_day_of_the_dead.glb?alt=media&token=32207436-c768-403f-879a-d43ce698d71f"} as ElementId;
   
   constructor() { }
 
