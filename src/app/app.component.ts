bb774869b06b6a3d33828989b1f3cdd68672ca7e
JoseLuisLanda <viajero13279@innovaccion.mx>
2022-12-18 22:49:07 -0600

    Changing first model
    sky usdz model as principal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,7 +10,7 @@ export class AppComponent implements OnInit, OnChanges  {
   @Output() item: EventEmitter<ElementId> = new EventEmitter<ElementId>();
   switchTemp: boolean  = false;
   title = 'Visualiz-AR';
-  itemAR:ElementId={uid:"Astronaut",name:"https://firebasestorage.googleapis.com/v0/b/visualizr.appspot.com/o/3DModels%2Flion_sculpture_processed.glb?alt=media&token=00e3d0fc-0744-4108-8f5a-702273c5471d"};
+  itemAR:ElementId={uid:"Astronaut",name:"../../../assets/models/sky.usdz"};//https://firebasestorage.googleapis.com/v0/b/visualizr.appspot.com/o/3DModels%2Flion_sculpture_processed.glb?alt=media&token=00e3d0fc-0744-4108-8f5a-702273c5471d"};
   ngOnChanges(changes: SimpleChanges): void {
     throw new Error('Method not implemented.');
   }
