e00743cf641ec2e5bd62645abe4cbb65737ae409
JoseLuisLanda <viajero13279@innovaccion.mx>
2023-01-02 02:31:28 -0600

    menu back

--- src/app/shared/afelement/multiMarkersName.js
+++ src/app/shared/afelement/multiMarkersName.js
@@ -39,7 +39,7 @@ AFRAME.registerComponent('markers_start',{
 			textEl.setAttribute('gesture-handler',{minScale: '0.25', maxScale: '10'});
 			textEl.setAttribute('geometry',{width:'4', height:'3'});
 			textEl.setAttribute('material',{src: '#menu-img', color: '#FFF'});
-			textEl.object3D.position.set(0, 1, 0);
+			textEl.object3D.position.set(0, .1, 0);
 			textEl.object3D.rotation.set(-90, 0, 0);
 
 /*
