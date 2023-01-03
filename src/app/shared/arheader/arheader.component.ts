d54343223b32b07be22fcb21c2da2060e8c2d193
JoseLuisLanda <viajero13279@innovaccion.mx>
2023-01-02 22:43:15 -0600

    fixing superheroes

--- src/app/shared/arheader/arheader.component.ts
+++ src/app/shared/arheader/arheader.component.ts
@@ -40,42 +40,36 @@ export class ARHeaderComponent implements OnInit, OnChanges {
     "../../../assets/models/captain_marvel.glb",//1.91                25 
     "../../../assets/models/2018_venom.glb",//10.89                   26
     "../../../assets/models/venom_marvel_super_war.glb",//2.07        27
-    "../../../assets/models/spider-man_-_hybrid_suit.glb",//1.24      28
+    "../../../assets/models/spider-man_-_hybrid_suit.glb",//1.24      28   16
 
     "../../../assets/models/ufo_doodle.glb",//9.12
     "../../../assets/models/alien.glb",//3.17
     "../../../assets/models/Astronaut.glb",//2.74
     "../../../assets/models/egyptian_pyramid.glb",//4.41
-    "../../../assets/models/jaguar_staircase_at_the_east_court_of_copan.glb",//11.22
+    "../../../assets/models/jaguar_staircase_at_the_east_court_of_copan.glb",//11.22    21
   
-  "../../../assets/models/ToyCar.glb",//5.74
-  "../../../assets/models/shishkebab.glb",//1.53
-  "../../../assets/models/RobotExpressive.glb",//520 kb
-   "../../../assets/models/santa_clauss.glb",//17.58
-  "../../../assets/models/catrina_girl.glb",//19.56
-
-  "../../../assets/models/altar_de_dia_de_muertos.glb",
-  "../../../assets/models/the_day_of_the_dead.glb",
+  "../../../assets/models/jaguar.glb",//    22
+  "../../../assets/models/puma_gray.glb",//   23
+  "../../../assets/models/jaguar_animation.glb",//    24
+ "../../../assets/models/mayan_jaguar.glb",//    25
   
-  "../../../assets/models/delorean_highpoly.glb",
-  "../../../assets/models/delorean_dmc_12.glb",
-  "../../../assets/models/nasi_lemak.glb",
-  "../../../assets/models/delorean__dmc-12.glb",
-  "../../../assets/models/parzivals_delorean_dmc-12.glb",
-  
-  "../../../assets/models/jaguar.glb",
-  "../../../assets/models/puma_gray.glb",
-  "../../../assets/models/jaguar_animation.glb",
- 
-  
-  "../../../assets/models/mayan_jaguar.glb",
-  
-  "../../../assets/models/piramide.glb",
+  "../../../assets/models/piramide.glb", //   26
   
   "../../../assets/models/ovni_low_poly.glb",
   "../../../assets/models/piramid.glb",
   
-  "../../../assets/models/plato_volador_amina.glb",
+  "../../../assets/models/plato_volador_amina.glb", //      29
+
+  "../../../assets/models/delorean_highpoly.glb",
+  "../../../assets/models/delorean_dmc_12.glb",
+  "../../../assets/models/nasi_lemak.glb",
+  "../../../assets/models/delorean__dmc-12.glb",
+  "../../../assets/models/parzivals_delorean_dmc-12.glb",//     34
+  "../../../assets/models/hoverboard.glb",
+  "../../../assets/models/hover_board_low_poly.glb",//      36
+  "../../../assets/models/marty_mcfly.glb",
+  "../../../assets/models/ironman.glb",//      38
+  "../../../assets/models/iron_n_r.glb",
 
   "../../../assets/models/t-shirt_m.glb",
   "../../../assets/models/t-shirt_mask.glb",
@@ -84,14 +78,24 @@ export class ARHeaderComponent implements OnInit, OnChanges {
   "../../../assets/models/skirt_and_t-shirt.glb",
   "../../../assets/models/std_t-shirt.glb",
   "../../../assets/models/tenis_surrado.glb",
-  "../../../assets/models/hoverboard.glb",
-  "../../../assets/models/hover_board_low_poly.glb",
+
   "../../../assets/models/charging_bull.glb",
   "../../../assets/models/bull_man.glb",
   "../../../assets/models/bull_for_3d_printing.glb",
   "../../../assets/models/bull_basemesh.glb",
   "../../../assets/models/blue_bull_miniature.glb",
-  "../../../assets/models/marty_mcfly.glb",
+  
+
+
+
+  "../../../assets/models/ToyCar.glb",//5.74
+  "../../../assets/models/shishkebab.glb",//1.53
+  "../../../assets/models/RobotExpressive.glb",//520 kb
+   "../../../assets/models/santa_clauss.glb",//17.58
+  "../../../assets/models/catrina_girl.glb",//19.56
+
+  "../../../assets/models/altar_de_dia_de_muertos.glb",
+  "../../../assets/models/the_day_of_the_dead.glb",
 ];
   constructor() { }
   ngOnChanges(changes: SimpleChanges): void {
