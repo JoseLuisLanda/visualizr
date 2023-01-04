302aad170606a9e3dc7687627eafb033ed4ebe9e
JoseLuisLanda <viajero13279@innovaccion.mx>
2023-01-04 09:54:21 -0600

    adding 18 patterns and images for tracker

--- src/app/shared/afelement/multiMarkersName.js
+++ src/app/shared/afelement/multiMarkersName.js
@@ -11,7 +11,7 @@ AFRAME.registerComponent('markers_start',{
 		var sceneEl = document.querySelector('a-scene');
 		
 		//list of the markers
-		for(var i=1; i<=3; i++)
+		for(var i=1; i<=18; i++)
 		{
 			var url="../../../assets/presets/pattern-Individual_Blocks-"+i+".patt";
 			markersURLArray.push(url);
@@ -19,7 +19,7 @@ AFRAME.registerComponent('markers_start',{
 			//console.log(url);
 		}
 
-		for(var k=0; k<3; k++)
+		for(var k=0; k<18; k++)
 		{
 			var markerEl = document.createElement('a-marker');
 			markerEl.setAttribute('type','pattern');
@@ -51,8 +51,8 @@ AFRAME.registerComponent('markers_start',{
 			//textEl.setAttribute('text',{color: 'red', align: 'center', value:markersNameArray[k], width: '5.5'});
 			textEl.setAttribute('id','image');
 			textEl.setAttribute('class','clickable');
-			textEl.setAttribute('gesture-handler',{minScale: '0.25', maxScale: '10'});
-			textEl.setAttribute('geometry',{width:'1', height:'2'});
+			textEl.setAttribute('gesture-handler',{minScale: '1', maxScale: '10'});
+			//textEl.setAttribute('geometry',{width:'1', height:'2'});
 			textEl.setAttribute('material',{src: '#menu'+[k], color: '#FFF'});
 			textEl.object3D.position.set(0, 0, 0);
 			textEl.object3D.rotation.set(-90, 0, 0);
